fa13aa75a8f27d2e7cc4e86fee458476f4eb8434
Caulicons <caulicons@hotmail.com>
2022-06-07 15:23:09 -0300

    fist commit - send github

---
+++ README.md
@@ -0,0 +1,7 @@
+# Que horas são ?
+
+como essa página você vai saber que horas são com um background personalizado e um imagem customizada.
+
+Fiz essa página enquanto estava aprendendo o básico do javascript. Particularmente aprendendo sobre o Date do javascript para captura a hora do aparelho conectador. 
+
+Meu tutor foi o grande Guanabara do canal Curso em Vídeo na sua playlist de vídeos de javascript básico patrocinado pelo google. 
---
+++ dinamismo.js
@@ -0,0 +1,38 @@
+function carregar(){
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('img')
+    let aps = window.document.getElementById('aps')
+    let data = new Date()
+    let hora =  data.getHours()
+    let min = data.getMinutes()
+
+    msg.innerHTML = (`${hora}:${min}`)
+
+    if (hora >= 0 && hora < 6){
+        aps.innerHTML = (`Boa Madrugada!`)
+        img.src = './imgs/madrugada.jpg'
+        document.body.style.cssText = 
+        'background: rgb(0, 60, 80)'
+    }
+    else if (hora >= 6 && hora < 12) {
+        aps.innerHTML = (`Bom dia !`)
+        img.src = './imgs/manha.jpg'
+        document.body.style.cssText = 
+        'background: rgb(233, 218, 86)' 
+    }
+    else if (hora >= 12 && hora < 18) {
+        aps.innerHTML = ( `Boa Tarde !`)
+        img.src = './imgs/tarde.jpg'
+        document.body.style.cssText = 
+        'background: rgb(253, 171, 19)'
+    }
+    else {
+        aps.innerHTML = (`Boa Noite !`)
+        img.src = './imgs/noite.jpg'
+        document.body.style.cssText = 
+        'background: rgb(17, 13, 6)'
+    }
+
+}
+
+carregar()
